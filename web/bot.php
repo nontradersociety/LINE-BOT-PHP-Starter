e6a4f4abebe0abd4bf6a487f4d29ea66fab6e4cb
nontradersociety <nce.wong@gmail.com>
2021-03-09 11:52:04 +0700

    Update bot.php

--- web/bot.php
+++ web/bot.php
@@ -332,6 +332,47 @@ if (!is_null($events['events'])) {
                 }
                 $case = 2;
             }
+		if ( strpos($textinput, 'ป้าเย') !== false || strpos($textinput, 'เยเลน') !== false || strpos($textinput, 'yellen') !== false || strpos($textinput, 'Yellen') !== false ) {
+                $rand = rand(0, 15);
+                if ($rand == 0) {
+                    $text = 'พาทองลงกี่จุดดีละ';
+                    $case = 1;
+                } elseif ($rand == 1) {
+                    $text = 'ได้ยินชื่อนี้แล้วเหมือนจะได้สัมผัสก้นเหว';
+                    $case = 1;
+                } elseif ($rand == 2) {
+                    $text = 'ให้ป้าพาดำดินหน่อยมั๊ย';
+                    $case = 1;
+                } elseif ($rand == 3) {
+                    $text = 'รูดซัก 1000 จุดเป็นยังไง';
+                    $case = 1;
+                } elseif ($rand == 4) {
+                    $img = 'https://static.onecms.io/wp-content/uploads/sites/20/2020/11/30/Janet-Yellen.jpg';
+                } elseif ($rand == 5) {
+                    img = 'https://api.time.com/wp-content/uploads/2016/09/gettyimages-495620136.jpg';
+                } elseif ($rand == 6) {
+                    img = 'https://static.timesofisrael.com/www/uploads/2020/11/Untitled-30.jpg';
+                } elseif ($rand == 7) {
+                    img = 'https://wgnradio.com/wp-content/uploads/sites/6/2021/03/4793d11a8dcb41a7a54bd20fc121cbbf.jpg?w=2560&h=1440&crop=1';
+                } elseif ($rand == 8) {
+                    img = 'https://image.cnbcfm.com/api/v1/image/104723997-GettyImages-850232670-janet-yellen.jpg?v=1614988149';
+                } elseif ($rand == 9) {
+                    img = 'https://news.bitcoin.com/wp-content/uploads/2019/09/yellen-bitcoin-speculative.jpg';
+                } elseif ($rand == 10) {
+                    img = 'https://images.barrons.com/im-263891?width=1280&size=1';
+                } elseif ($rand == 11) {
+                    img = 'https://static01.nyt.com/images/2016/02/11/business/11fed3/11fed3-superJumbo.jpg';
+                } elseif ($rand == 12) {
+                    img = 'https://cms.qz.com/wp-content/uploads/2020/11/2017-12-13T201246Z_930034851_RC1D481B33A0_RTRMADP_3_USA-FED-YELLEN-e1606245807705.jpg?quality=75&strip=all&w=1200&h=900&crop=1';
+                } elseif ($rand == 13) {
+                    img = 'https://i.insider.com/5fbff22b037cbd0018612811';
+                } elseif ($rand == 14) {
+                    img = 'https://images.barrons.com/im-264983?width=1280&size=1';
+                } else {
+                    img = 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Janet_Yellen_official_Federal_Reserve_portrait.jpg';
+                }
+                $case = 2;
+            }
             if ( strpos($textinput, 'พาวเวล') !== false || strpos($textinput, 'powell') !== false  || strpos($textinput, 'Powell') !== false ) {
                 $rand = rand(0, 14);
                 if ($rand == 0) {
@@ -1295,16 +1336,40 @@ if (!is_null($events['events'])) {
                 $case = 1;
             }
             if ( strpos($textinput, 'สึด') !== false || strpos($textinput, 'สึส') !== false || strpos($textinput, 'คุวย') !== false || strpos($textinput, 'สึด') !== false || strpos($textinput, 'ไอเวร') !== false || strpos($textinput, 'ไอเวน') !== false ) {
-                $rand = rand(0, 5);
+                $rand = rand(0, 7);
                 if ($rand == 0) {
                     $text = 'พูดให้มันดีๆหน่อยได้ป่ะ';
                 } elseif ($rand == 1) {
-                    $text = 'อย่ามากร่างแถวนี้เดี๋ยวจะโดนตบเอา';
+                    $array = [
+                        "id" => "11537",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "52002773"
+                    ];
+                    $case = 4;
                 } elseif ($rand == 2) {
-                    $text = 'พูดคำหยาบนี่ยังไง อยากใส่เดี่ยวว่างั้น';
+                    $array = [
+                        "id" => "11537",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "52002767"
+                    ];
+                    $case = 4;
                 } elseif ($rand == 3) {
-                    $text = 'ปากดีจัง';
+                    $array = [
+                        "id" => "11537",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "52002772"
+                    ];
+                    $case = 4;
                 } elseif ($rand == 4) {
+                    $text = 'อย่ามากร่างแถวนี้เดี๋ยวจะโดนตบเอา';
+                } elseif ($rand == 5) {
+                    $text = 'พูดคำหยาบนี่ยังไง อยากใส่เดี่ยวว่างั้น';
+                } elseif ($rand == 6) {
+                    $text = 'ปากดีจัง';
+                } elseif ($rand == 7) {
                     $text = 'เคยพิมพ์อยู่แล้วหงายหลังป่ะ ???';
                 }
                 $case = 1;
@@ -1456,6 +1521,30 @@ https://th.investing.com/crypto/';
                         "type" => "sticker",
                         "packageId" => "2",
                         "stickerId" => "163"
+                    ];
+                    $case = 4;
+                } elseif ($rand == 4) {
+                    $array = [
+                        "id" => "11538",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "51626504"
+                    ];
+                    $case = 4;
+                } elseif ($rand == 5) {
+                    $array = [
+                        "id" => "11538",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "51626512"
+                    ];
+                    $case = 4;
+                } elseif ($rand == 6) {
+                    $array = [
+                        "id" => "11538",
+                        "type" => "sticker",
+                        "packageId" => "1",
+                        "stickerId" => "51626498"
                     ];
                     $case = 4;
                 }
